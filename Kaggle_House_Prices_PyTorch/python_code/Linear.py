1bf43cf3e465ee607674527fe220a790a0b16aec
Evergreen0929 <90333984+Evergreen0929@users.noreply.github.com>
2021-11-30 03:05:08 +0800

    Add files via upload

---
+++ Kaggle_House_Prices_PyTorch/python_code/Linear.py
@@ -0,0 +1,172 @@
+
+# coding: utf-8
+
+# In[31]:
+
+
+#get_ipython().magic('matplotlib inline')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from torch import nn
+from torch.utils import data
+
+from model import Mlp, TF
+
+
+# ### Import Data
+train_raw = pd.read_csv('train.csv')
+test_raw = pd.read_csv('test.csv')
+
+
+# ##### description
+print(train_raw.shape, test_raw.shape)
+print(train_raw.iloc[0:4,[0,1,2,3,-3,-2,-1]])
+
+
+# ##### Concatenate the train and test data for standardisation
+all_features_raw = pd.concat((train_raw.iloc[:,1:-1], test_raw.iloc[:,1:]))
+all_features = all_features_raw.copy()
+
+
+# ### Data Preprocessing
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(
+    lambda x: (x-x.mean())/(x.std()))
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
+
+# ##### Get Dummies
+all_features = pd.get_dummies(all_features, dummy_na=True)
+print(all_features.shape)
+#print(all_features)
+
+
+# ###### Convert to tensors
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+n_train = train_raw.shape[0]
+train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float32).to(device)
+test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float32).to(device)
+train_labels = torch.tensor(train_raw.SalePrice.values.reshape(-1,1), dtype=torch.float32).to(device)
+
+
+# ### Training
+
+# ##### Define loss function and model
+
+# ##### We can experiement with the model. For example, the simplest can be a single nn.Linear model
+
+loss = nn.MSELoss()
+in_features = train_features.shape[1]
+
+def get_net():
+    #net = nn.Sequential(nn.Linear(in_features, 64), nn.ReLU(), nn.Linear(64,1)).to(device)
+    net = TF(in_features=331, drop=0.).to(device)
+    return net
+
+
+# ##### Log root mean squared error
+def log_rmse(net, features, labels):
+    clipped_preds = torch.clamp(net(features), 1, float('inf'))
+    rmse = torch.sqrt(loss(torch.log(clipped_preds), torch.log(labels)))
+    return rmse.item()
+
+
+# ##### Load dataset
+
+def load_array(data_arrays, batch_size, is_train=True):
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+# ##### Define trainning model
+def train(net, train_features, train_labels, test_features, test_labels,
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    train_iter = load_array((train_features, train_labels), batch_size)
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate,
+                                 weight_decay=weight_decay)
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            optimizer.zero_grad()
+            l = loss(net(X), y)
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+
+# ##### K-fold cross-validation
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j*fold_size, (j+1)*fold_size)
+        X_part, y_part = X[idx,:], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat([X_train, X_part], 0)
+            y_train = torch.cat([y_train, y_part], 0)
+    return X_train, y_train, X_valid, y_valid
+
+
+def k_fold(k, X_train, y_train, num_epochs, learning_rate, weight_decay,
+           batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate,
+                                   weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        print(f'fold {i + 1}, train log rmse {float(train_ls[-1]):f}, '
+              f'valid log rmse {float(valid_ls[-1]):f}')
+    return train_l_sum / k, valid_l_sum / k
+
+
+def train_and_pred(train_features, test_features, train_labels, test_data, 
+                   num_epochs, lr, weight_decay, batch_size):
+    net = get_net()
+    train_ls, valid_ls = train(net, train_features, train_labels, None, None,
+                        num_epochs, lr, weight_decay, batch_size)
+    train_ls_all = []
+    train_ls_all.append(train_ls)
+    # plt.plot(np.arange(1,101,1),train_ls_all[0])
+    # plt.xlabel('epoch'), plt.ylabel('rmse')
+    print(f'train log rmse {float(train_ls[-1]):f}')
+    preds = net(test_features).cpu().detach().numpy()
+    test_data['SalePrice'] = pd.Series(preds.reshape(1,-1)[0])
+    submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    submission.to_csv('submission.csv', index=False)
+
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100, 0.0006, 2, 64
+
+print("K-fold Validation:\n")
+
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr,
+                          weight_decay, batch_size)
+print(f'{k}-fold validation: avg train log rmse: {float(train_l):f}, '
+      f'avg valid log rmse: {float(valid_l):f}')
+
+# train_and_pred(train_features, test_features, train_labels, test_raw,
+#                num_epochs, lr, weight_decay, batch_size)
+
+print("\n\nTrain on different lr range from 0.01 to 0.0001 with stepping 0.0001:\n")
+
+lr_range = np.arange(0.0001, 0.01, 0.0001).tolist()
+for lr in lr_range:
+    print('\nlr:{:.5f}'.format(lr))
+    train_and_pred(train_features, test_features, train_labels, test_raw,
+               num_epochs, lr, weight_decay, batch_size)
+
+
---
+++ Kaggle_House_Prices_PyTorch/python_code/model.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, act_layer=nn.GELU, drop=0., pred=True):
+        super().__init__()
+        #out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.q = nn.Linear(in_features, in_features)
+        self.k = nn.Linear(in_features, in_features)
+        self.v = nn.Linear(in_features, in_features)
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.pred = pred
+        if pred==True:
+            self.fc2 = nn.Linear(hidden_features,1)
+        else:
+            self.fc2 = nn.Linear(hidden_features, in_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x0 = x
+        q = self.q(x).unsqueeze(2)
+        k = self.k(x).unsqueeze(2)
+        v = self.v(x).unsqueeze(2)
+        attn = (q @ k.transpose(-2, -1))
+        #print(attn.size())
+        attn = attn.softmax(dim=-1)
+        x = (attn @ v).squeeze(2)
+        #print(x.size())
+        x += x0
+        x1 = x
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        if self.pred==False:
+            x += x1
+
+        x = x.squeeze(0)
+
+        return x
+
+
+class TF(nn.Module):
+    def __init__(self, in_features, drop=0.):
+        super().__init__()
+        self.Block1 = Mlp(in_features=in_features, hidden_features=331, act_layer=nn.GELU, drop=drop, pred=False)
+        # self.Block1_1 = Mlp(in_features=in_features, hidden_features=64, act_layer=nn.GELU, drop=drop, pred=False)
+        # self.Block1_2 = Mlp(in_features=in_features, hidden_features=64, act_layer=nn.GELU, drop=drop, pred=False)
+        # self.Block1_3 = Mlp(in_features=in_features, hidden_features=64, act_layer=nn.GELU, drop=drop, pred=False)
+        # self.Block1_1 = Mlp(in_features=in_features, hidden_features=64, act_layer=nn.GELU, drop=drop, pred=False)
+        # self.Block1_1 = Mlp(in_features=in_features, hidden_features=64, act_layer=nn.GELU, drop=drop, pred=False)
+        # self.Block1_1 = Mlp(in_features=in_features, hidden_features=64, act_layer=nn.GELU, drop=drop, pred=False)
+        self.Block2 = Mlp(in_features=in_features, hidden_features=331, act_layer=nn.GELU, drop=drop, pred=True)
+
+    def forward(self, x):
+        return self.Block2(self.Block1(x))
